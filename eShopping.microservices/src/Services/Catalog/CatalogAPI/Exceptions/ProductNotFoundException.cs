267beebef973711af264b10094e8bce4ac47675d
manulaPankaja <pankajabad1998@gmail.com>
2025-05-29 01:05:29 +0530

    get product by id

---
+++ eShopping.microservices/src/Services/Catalog/CatalogAPI/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace CatalogAPI.Exceptions
+{
+    public class ProductNotFoundException : Exception
+    {
+        public ProductNotFoundException() : base("Product not found!")
+        { 
+
+        }
+    }
+}
