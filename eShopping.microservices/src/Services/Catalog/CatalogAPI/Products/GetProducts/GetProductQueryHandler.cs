dca6edeb29ce3420a06132d8557711ba578039dc
manulaPankaja <pankajabad1998@gmail.com>
2025-06-26 01:54:48 +0530

    pagination - pagination using mediatR

--- eShopping.microservices/src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductQueryHandler.cs
+++ eShopping.microservices/src/Services/Catalog/CatalogAPI/Products/GetProducts/GetProductQueryHandler.cs
@@ -1,13 +1,13 @@
 ﻿namespace CatalogAPI.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductResult>;
     public record GetProductResult(IEnumerable<Product> Products);
     internal class GetProductQueryHandler(IDocumentSession session)
         : IQueryHandler<GetProductsQuery, GetProductResult>
     {
         public async Task<GetProductResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>().ToPagedListAsync(query.PageNumber ?? 1,  query.PageSize ?? 10, cancellationToken);
 
             return new GetProductResult(products);
         }
