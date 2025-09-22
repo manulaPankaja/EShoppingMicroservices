c99baf4b6c6ac2bb10e3f6a8a30d23a822a424d2
manulaPankaja <pankajabad1998@gmail.com>
2025-09-22 22:35:44 +0530

    development - create two models for basket service

---
+++ eShopping.microservices/src/Services/Basket/BasketAPI/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace BasketAPI.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCardItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        //Required for Mapping
+        public ShoppingCart() 
+        { 
+        }
+    }
+}
