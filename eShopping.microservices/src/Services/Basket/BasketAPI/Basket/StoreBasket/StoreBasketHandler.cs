0fb9a515a187039b8b7ed6b42d3351e4a58f20f4
manulaPankaja <pankajabad1998@gmail.com>
2025-10-12 23:23:19 +0530

    development - store basket endpoint and handler

--- eShopping.microservices/src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
+++ eShopping.microservices/src/Services/Basket/BasketAPI/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,26 @@
-﻿namespace BasketAPI.Basket.StoreBasket
+﻿namespace BasketAPI.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string userName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
-    public class StoreBasketHandler
+    public StoreBasketCommandValidator()
     {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("username is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+
+        //TODO - store in database (use Marten upsert - if exist = update, if not exist = create
+        //TODO - update cache
+
+        return new StoreBasketResult("swn");
     }
 }
